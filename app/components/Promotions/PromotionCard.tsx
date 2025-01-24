843906bd0bbb1e22476158b10069d08a45d9e216
Daniel Martins <martinsda@gmail.com>
2025-01-24 19:32:15 -0300

    add promotions

---
+++ app/components/Promotions/PromotionCard.tsx
@@ -0,0 +1,22 @@
+import { StaticImport } from "next/dist/shared/lib/get-img-props";
+import Image from "next/image";
+import { FC } from "react";
+import CenteredEl from "../ui/CenteredElement";
+
+type PromotionCardProps = {
+  img: StaticImport;
+};
+
+const PromotionCard: FC<PromotionCardProps> = ({ img }) => {
+  return (
+    <CenteredEl className="w-full h-80 rounded-xl overflow-hidden hover:cursor-pointer bg-red-100">
+      <Image
+        className="w-full h-80 hover:scale-[103%] transition-transform ease-in-out duration-300"
+        src={img}
+        alt="image card"
+      />
+    </CenteredEl>
+  );
+};
+
+export default PromotionCard;
--- app/components/Promotions/index.tsx
+++ app/components/Promotions/index.tsx
@@ -1,5 +1,34 @@
+import SectionHeader from "../SectionHeader/SectionHeader";
+import CenteredEl from "../ui/CenteredElement";
+
+import Promotion0 from "../../assets/images/promotions/promotion-0.png";
+import Promotion1 from "../../assets/images/promotions/promotion-1.png";
+import Promotion2 from "../../assets/images/promotions/promotion-2.png";
+import Promotion3 from "../../assets/images/promotions/promotion-3.png";
+import Promotion4 from "../../assets/images/promotions/promotion-4.png";
+import Promotion5 from "../../assets/images/promotions/promotion-5.png";
+import PromotionCard from "./PromotionCard";
+
+const imgData = [
+  Promotion0,
+  Promotion1,
+  Promotion2,
+  Promotion3,
+  Promotion4,
+  Promotion5,
+];
+
 const Promotions = () => {
-  return <div>Promotions</div>;
+  return (
+    <CenteredEl className="my-10" direction="col">
+      <SectionHeader title="Promoções" />
+      <CenteredEl className="grid grid-cols-2 gap-x-2">
+        {imgData.slice(0, 2).map((img, index) => (
+          <PromotionCard key={index} img={img} />
+        ))}
+      </CenteredEl>
+    </CenteredEl>
+  );
 };
 
 export default Promotions;
