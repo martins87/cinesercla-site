86323803c106d8276f74bd984c2117564ee9ed1e
Daniel Martins <martinsda@gmail.com>
2025-01-30 12:36:49 -0300

    add bomboniere page

---
+++ app/(pages)/bomboniere/page.tsx
@@ -0,0 +1,13 @@
+import BgImageContainer from "@/app/components/ui/BgImageContainer";
+
+import Bg from "../../assets/images/backgrounds/bomboniere.png";
+
+const Bomboniere = () => {
+  return (
+    <>
+      <BgImageContainer img={Bg} title="Bomboniere" />
+    </>
+  );
+};
+
+export default Bomboniere;
