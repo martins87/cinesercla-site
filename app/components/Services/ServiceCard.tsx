e27386ec53cf28cd559a49ff5c78beaf99ca4d93
Daniel Martins <martinsda@gmail.com>
2025-02-04 16:47:14 -0300

    edit service card styling

--- app/components/Services/ServiceCard.tsx
+++ app/components/Services/ServiceCard.tsx
@@ -4,6 +4,7 @@ import { StaticImport } from "next/dist/shared/lib/get-img-props";
 import Typography from "../Typography";
 import CenteredEl from "../ui/CenteredElement";
 import OuterLinkArrow from "./OuterLinkArrow";
+import Link from "next/link";
 
 type ServiceCardProps = {
   imgData: { label: string; img: StaticImport };
@@ -12,19 +13,21 @@ type ServiceCardProps = {
 const ServiceCard: FC<ServiceCardProps> = ({ imgData }) => {
   const { label, img } = imgData;
   return (
-    <div className="group w-full h-[500px] relative object-center rounded-xl overflow-hidden hover:cursor-pointer">
-      <Image
-        className="absolute inset-0 object-cover h-full group-hover:scale-[103%] transition-all ease-in-out duration-300"
-        src={img}
-        alt="image card"
-      />
-      <CenteredEl className="h-[15%] absolute bottom-0 left-0 bg-black/20 backdrop-blur-md">
-        <CenteredEl className="px-8" justify="between">
-          <Typography>{label}</Typography>
-          <OuterLinkArrow />
+    <Link href={"/corporativo"}>
+      <div className="group w-full h-[500px] relative object-center rounded-xl overflow-hidden hover:cursor-pointer">
+        <Image
+          className="absolute inset-0 object-cover h-full group-hover:scale-[103%] transition-all ease-in-out duration-300"
+          src={img}
+          alt="image card"
+        />
+        <CenteredEl className="h-[15%] absolute bottom-0 left-0 bg-black/20 backdrop-blur-md">
+          <CenteredEl className="px-8" justify="between">
+            <Typography className="text-lg tracking-wide">{label}</Typography>
+            <OuterLinkArrow />
+          </CenteredEl>
         </CenteredEl>
-      </CenteredEl>
-    </div>
+      </div>
+    </Link>
   );
 };
 
