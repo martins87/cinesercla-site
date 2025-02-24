83708cb79e1c3f60d852babf442499c484e6e21a
Daniel Martins <martinsda@gmail.com>
2025-02-24 19:40:26 -0300

    edit leis page

--- app/(pages)/leis/page.tsx
+++ app/(pages)/leis/page.tsx
@@ -1,11 +1,23 @@
 import BgImageContainer from "@/app/components/ui/BgImageContainer";
 
+import Container from "@/app/components/ui/Container";
+import Centered from "@/app/components/ui/CenteredElement";
+import Typography from "@/app/components/Typography";
+import Button from "@/app/components/ui/Button";
 import Bg from "../../assets/images/backgrounds/leis.png";
 
 const Leis = () => {
   return (
     <>
       <BgImageContainer img={Bg} title="Leis" />
+      <Container className="h-fit bg-[#EAEAEA] mt-16">
+        <Centered className="gap-y-4" direction="col">
+          <Typography className="text-2xl text-black/90">
+            Precisa de mais ajuda?
+          </Typography>
+          <Button label="ENTRE EM CONTATO" primary />
+        </Centered>
+      </Container>
     </>
   );
 };
