ae39545e851191757017d0562b1edfbcb34ca425
Daniel Martins <martinsda@gmail.com>
2025-03-01 00:10:11 -0300

    code cleanup

---
+++ app/components/FAQ/FAQList.tsx
@@ -0,0 +1,47 @@
+import React, { FC } from "react";
+
+import { FAQItem } from "@/app/types/FAQItem";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+
+type FAQListProps = {
+  faqArray: FAQItem[];
+  itemIndex: number;
+  secondColumn?: boolean;
+  handleClick: (index: number) => void;
+};
+
+const FAQList: FC<FAQListProps> = ({
+  faqArray,
+  itemIndex,
+  secondColumn,
+  handleClick,
+}) => {
+  const value = secondColumn ? 4 : 0;
+
+  return (
+    <Accordion
+      className="w-full"
+      type="single"
+      collapsible
+      value={`item-${itemIndex}`}
+    >
+      {faqArray.map((item: FAQItem, index: number) => (
+        <AccordionItem
+          key={index}
+          value={`item-${index + value}`}
+          onClick={() => handleClick(index + value)}
+        >
+          <AccordionTrigger>{item.question}</AccordionTrigger>
+          <AccordionContent>{item.answer}</AccordionContent>
+        </AccordionItem>
+      ))}
+    </Accordion>
+  );
+};
+
+export default FAQList;
--- app/components/FAQ/index.tsx
+++ app/components/FAQ/index.tsx
@@ -3,14 +3,8 @@
 import React, { useState } from "react";
 import { useMediaQuery } from "react-responsive";
 
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from "@/components/ui/accordion";
-import { FAQItem } from "@/app/types/FAQItem";
 import { FAQData } from "@/app/constants/faq";
+import FAQList from "./FAQList";
 
 const FAQ = () => {
   const ismobile = useMediaQuery({ query: "(max-width: 768px)" });
@@ -25,61 +19,24 @@ const FAQ = () => {
   return (
     <div className="w-full flex gap-x-4">
       {ismobile ? (
-        <Accordion
-          className="w-full"
-          type="single"
-          collapsible
-          value={`item-${itemIndex}`}
-        >
-          {faq.map((item: FAQItem, index: number) => (
-            <AccordionItem
-              key={index}
-              value={`item-${index}`}
-              onClick={() => handleClick(index)}
-            >
-              <AccordionTrigger>{item.question}</AccordionTrigger>
-              <AccordionContent>{item.answer}</AccordionContent>
-            </AccordionItem>
-          ))}
-        </Accordion>
+        <FAQList
+          faqArray={faq}
+          handleClick={handleClick}
+          itemIndex={itemIndex}
+        />
       ) : (
         <>
-          <Accordion
-            className="w-full"
-            type="single"
-            collapsible
-            value={`item-${itemIndex}`}
-          >
-            {faq.slice(0, 3).map((item: FAQItem, index: number) => (
-              <AccordionItem
-                key={index}
-                value={`item-${index}`}
-                onClick={() => handleClick(index)}
-              >
-                <AccordionTrigger>{item.question}</AccordionTrigger>
-                <AccordionContent>{item.answer}</AccordionContent>
-              </AccordionItem>
-            ))}
-          </Accordion>
-          <Accordion
-            className="w-full"
-            type="single"
-            collapsible
-            value={`item-${itemIndex}`}
-          >
-            {faq
-              .slice(3, FAQData.length)
-              .map((item: FAQItem, index: number) => (
-                <AccordionItem
-                  key={index}
-                  value={`item-${index + 4}`}
-                  onClick={() => handleClick(index + 4)}
-                >
-                  <AccordionTrigger>{item.question}</AccordionTrigger>
-                  <AccordionContent>{item.answer}</AccordionContent>
-                </AccordionItem>
-              ))}
-          </Accordion>
+          <FAQList
+            faqArray={faq.slice(0, 3)}
+            handleClick={handleClick}
+            itemIndex={itemIndex}
+          />
+          <FAQList
+            faqArray={faq.slice(3, faq.length)}
+            handleClick={handleClick}
+            itemIndex={itemIndex}
+            secondColumn
+          />
         </>
       )}
     </div>
