6c2950a1637712e775394fd785bb80c46d0bafe1
Daniel Martins <martinsda@gmail.com>
2025-03-07 13:24:31 -0300

    add link to movie page

--- app/components/Movies/MovieCard.tsx
+++ app/components/Movies/MovieCard.tsx
@@ -1,5 +1,6 @@
 import { FC } from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 import { Movie } from "@/app/types/Movie";
 import GlassCard from "./GlassCard";
@@ -12,32 +13,34 @@ type MovieCardProps = {
 
 const MovieCard: FC<MovieCardProps> = ({ movie }) => {
   return (
-    <div className="flex flex-col gap-y-2 bg-[#3B3B3B] rounded-2xl p-4">
-      <div className="relative min-h-72 max-h-72 lg:min-h-80 lg:max-h-80 xl:min-h-96 xl:max-h-96 flex flex-1 object-center">
-        <Image
-          className="w-full rounded-xl object-cover"
-          src={movie.cover!}
-          alt="movie cover"
-        />
-        <GlassCard label={movie.status} />
-      </div>
-      <div className="flex items-center justify-between">
-        <div className="w-1/2 flex items-center">
-          <Typography className="text-lg font-normal" maxChar={24}>
-            {movie.title}
-          </Typography>
+    <Link href="/movie/10">
+      <div className="flex flex-col gap-y-2 bg-[#3B3B3B] rounded-2xl p-4">
+        <div className="relative min-h-72 max-h-72 lg:min-h-80 lg:max-h-80 xl:min-h-96 xl:max-h-96 flex flex-1 object-center">
+          <Image
+            className="w-full rounded-xl object-cover"
+            src={movie.cover!}
+            alt="movie cover"
+          />
+          <GlassCard label={movie.status} />
         </div>
-        <div className="w-1/2 flex flex-col gap-y-1 items-end">
-          <Typography className="text-xl font-normal opacity-50">
-            {movie.genre}
-          </Typography>
-          <div className="flex items-center justify-between gap-x-2">
-            <Image width={44} src={IMDBLogo} alt="imdb logo" />
-            <Typography>{movie.imdbRate}</Typography>
+        <div className="flex items-center justify-between">
+          <div className="w-1/2 flex items-center">
+            <Typography className="text-lg font-normal" maxChar={24}>
+              {movie.title}
+            </Typography>
+          </div>
+          <div className="w-1/2 flex flex-col gap-y-1 items-end">
+            <Typography className="text-xl font-normal opacity-50">
+              {movie.genre}
+            </Typography>
+            <div className="flex items-center justify-between gap-x-2">
+              <Image width={44} src={IMDBLogo} alt="imdb logo" />
+              <Typography>{movie.imdbRate}</Typography>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
