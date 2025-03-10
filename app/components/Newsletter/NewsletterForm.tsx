bf280bb4b75fdd5e1cc7a775afe43f55aa205536
Daniel Martins <martinsda@gmail.com>
2025-03-10 15:52:23 -0300

    edit newsletter responsiveness

--- app/components/Newsletter/NewsletterForm.tsx
+++ app/components/Newsletter/NewsletterForm.tsx
@@ -1,16 +1,14 @@
 import Typography from "../Typography";
 import CenteredEl from "../ui/CenteredElement";
 import Input from "../Input";
-import User from "../../assets/icons/user.svg";
-import Email from "../../assets/icons/email.svg";
 import Capcha from "../Capcha";
 
 const NewsletterForm = () => {
   return (
     <CenteredEl className="gap-y-2" direction="col">
       <CenteredEl className="gap-x-2">
-        <Input icon={User} placeholder="Nome" />
-        <Input icon={Email} placeholder="Email" />
+        <Input placeholder="Nome*" />
+        <Input placeholder="Email*" />
       </CenteredEl>
       <CenteredEl className="py-4 rounded-xl bg-[#980038]">
         <Typography className="text-2xl" font="gellix">
--- app/components/Newsletter/index.tsx
+++ app/components/Newsletter/index.tsx
@@ -3,37 +3,39 @@ import Image from "next/image";
 import MoviesPoster from "../../assets/images/movies-poster.png";
 import Typography from "../Typography";
 import NewsletterForm from "./NewsletterForm";
-import CenteredEl from "../ui/CenteredElement";
+import CenteredElement from "../ui/CenteredElement";
 
 const Newsletter = () => {
   return (
-    <CenteredEl className="my-10" direction="col">
-      <CenteredEl className="relative rounded-2xl overflow-hidden z-0 px-10 py-28">
+    <CenteredElement className="my-10" direction="col">
+      <CenteredElement className="relative rounded-2xl overflow-hidden z-0 px-10 py-28">
         <Image
-          className="absolute top-0 left-0"
+          className="absolute top-0 left-0 h-full"
           src={MoviesPoster}
           alt="movies poster"
         />
-        <CenteredEl className="text-white z-10">
-          <CenteredEl
-            className="w-1/2"
-            items="start"
+        <CenteredElement className="flex-col tablet:flex-row gap-y-10 text-white z-10">
+          <CenteredElement
+            className="items-center tablet:items-start tablet:w-1/2"
             direction="col"
             justify="around"
           >
-            <Typography className="text-5xl" weight="800">
+            <Typography className="text-4xl tablet:text-5xl" weight="800">
               RECEBA NOSSA
             </Typography>
-            <Typography className="text-5xl text-[#D61E43]" weight="800">
+            <Typography
+              className="text-4xl tablet:text-5xl text-[#D61E43]"
+              weight="800"
+            >
               PROGRAMAÇÃO
             </Typography>
-          </CenteredEl>
-          <CenteredEl className="w-1/2" direction="col">
+          </CenteredElement>
+          <CenteredElement className="tablet:w-1/2" direction="col">
             <NewsletterForm />
-          </CenteredEl>
-        </CenteredEl>
-      </CenteredEl>
-    </CenteredEl>
+          </CenteredElement>
+        </CenteredElement>
+      </CenteredElement>
+    </CenteredElement>
   );
 };
 
