ad8e748bbd4a114ca05a762ab4bb8e60c9dc7101
Daniel Martins <martinsda@gmail.com>
2025-03-11 08:13:55 -0300

    edit captcha styling

--- app/components/Capcha.tsx
+++ app/components/Capcha.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 
 const Capcha = () => {
   return (
-    <CenteredEl className="w-[302px] h-[74px] p-2 bg-[#FAFAFA] border border-[#D6D6D6] rounded-r-sm mr-auto">
+    <CenteredEl className="w-full h-[74px] bg-[#FAFAFA] border border-[#D6D6D6] rounded-r-sm mr-auto">
       <CenteredEl className="gap-x-2 justify-start flex flex-1">
         <div className="w-6 aspect-square bg-white border-2 border-[#C1C1C1] rounded-sm"></div>
         <Typography className="text-black">I&apos;m not a robot</Typography>
