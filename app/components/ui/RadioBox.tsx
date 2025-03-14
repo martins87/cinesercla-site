f75b4552bd664ce79650ce87f43a711db23e1f2d
Daniel Martins <martinsda@gmail.com>
2025-03-13 21:23:24 -0300

    add radiobox component

---
+++ app/components/ui/RadioBox.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { FC, useState } from "react";
+
+import Typography from "../Typography";
+import CenteredElement from "./CenteredElement";
+
+type RadioBoxProps = {
+  label: string;
+};
+
+const RadioBox: FC<RadioBoxProps> = ({ label }) => {
+  const [clicked, setClicked] = useState(false);
+
+  return (
+    <CenteredElement
+      className="h-20 px-6 py-4 hover:cursor-pointer bg-[#A3A3A3] hover:bg-[#A3A3A3]/90 rounded-xl"
+      onClick={() => setClicked(!clicked)}
+      items="center"
+      justify="between"
+    >
+      <Typography className="text-xl uppercase" weight="700">
+        {label}
+      </Typography>
+      {!clicked ? (
+        <div className="w-4 aspect-square border-2 border-white rounded-full mr-2" />
+      ) : (
+        <CenteredElement className="w-fit p-2 bg-[#980038]/25 rounded-full">
+          <div className="w-4 aspect-square bg-[#980038] rounded-full" />
+        </CenteredElement>
+      )}
+    </CenteredElement>
+  );
+};
+
+export default RadioBox;
