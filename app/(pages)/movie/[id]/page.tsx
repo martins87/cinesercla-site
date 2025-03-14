cd62202e4e1fcc9f04fc6f8fd78e3baa3d073057
Daniel Martins <martinsda@gmail.com>
2025-03-13 22:12:28 -0300

    add trailer video to movie page

--- app/(pages)/movie/[id]/page.tsx
+++ app/(pages)/movie/[id]/page.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { useState } from "react";
 import Image from "next/image";
 
 import GradientOverlay from "@/app/components/GradientOverlay";
@@ -8,27 +11,36 @@ import MovieTrailers from "@/app/components/Movie/MovieTrailers";
 import { movie } from "@/app/constants/movies";
 import play from "@/app/assets/icons/play.svg";
 import CenteredElement from "@/app/components/ui/CenteredElement";
+import TrailerVideo from "@/app/components/Movie/TrailerVideo";
 
-// @ts-expect-error:next-line
-const MoviePage = async ({ params }) => {
-  const { id } = await params;
-  console.log(id);
+const MoviePage = () => {
+  // const { id } = params;
+  // console.log(id);
+  const [playing, setPlaying] = useState(false);
 
   return (
     <>
-      <CenteredElement className="relative h-screen">
-        <Image
-          className="w-screen h-screen object-cover tablet:object-fill"
-          src={movie.cover!}
-          alt="Image"
+      {playing ? (
+        <TrailerVideo
+          src="https://www.youtube.com/embed/AFEaeuyOUBw?si=xW3npUSlt_YNgjKX&autoplay=1"
+          setPlaying={setPlaying}
         />
-        <Image
-          className="absolute top-1/2 opacity-75 -m-10"
-          src={play}
-          alt="play icon"
-        />
-        <GradientOverlay hero />
-      </CenteredElement>
+      ) : (
+        <CenteredElement className="relative h-screen">
+          <Image
+            className="w-screen h-screen object-cover tablet:object-fill transition-all duration-300 ease-in-out"
+            src={movie.cover!}
+            alt="Image"
+          />
+          <Image
+            className="absolute top-1/2 opacity-75 -m-10 hover:cursor-pointer hover:scale-105 transition-all duration-300 ease-in-out"
+            src={play}
+            alt="play icon"
+            onClick={() => setPlaying(true)}
+          />
+          <GradientOverlay hero />
+        </CenteredElement>
+      )}
       <Container className="mt-10 gap-y-10">
         <MovieInfo movie={movie} />
         <MovieSession movie={movie} />
---
+++ app/components/Movie/TrailerVideo.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { Dispatch, FC, SetStateAction, useEffect } from "react";
+import CenteredElement from "../ui/CenteredElement";
+
+type TrailerVideoProps = {
+  src: string;
+  setPlaying: Dispatch<SetStateAction<boolean>>;
+};
+
+const TrailerVideo: FC<TrailerVideoProps> = ({ src, setPlaying }) => {
+  useEffect(() => {
+    const closeModal = (e: KeyboardEvent) => {
+      if (e.key === "Escape") {
+        setPlaying(false);
+      }
+    };
+
+    window.addEventListener("keydown", closeModal);
+
+    return () => window.removeEventListener("keydown", closeModal);
+  }, [setPlaying]);
+
+  return (
+    <CenteredElement className="w-screen h-screen bg-black z-10">
+      <div className="absolute w-[80vw] h-[80vh] -mt-20 tablet:-mt-6">
+        <iframe
+          src={src}
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+          title="YouTube Video"
+          className="absolute top-0 left-0 w-full h-full"
+        ></iframe>
+      </div>
+    </CenteredElement>
+  );
+};
+
+export default TrailerVideo;
