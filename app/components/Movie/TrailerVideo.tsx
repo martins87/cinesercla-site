f76e7fa5d22ed14248d29387dae3a068b42f9836
Daniel Martins <martinsda@gmail.com>
2025-04-21 14:34:38 -0300

    add close icon to trailer component

--- app/components/Movie/TrailerVideo.tsx
+++ app/components/Movie/TrailerVideo.tsx
@@ -1,8 +1,10 @@
 "use client";
 
 import { Dispatch, FC, SetStateAction, useEffect } from "react";
+import Image from "next/image";
 import { twMerge } from "tailwind-merge";
-import CenteredElement from "../ui/CenteredElement";
+
+import close from "@/app/assets/icons/close.svg";
 
 type TrailerVideoProps = {
   videoKey: string;
@@ -27,25 +29,32 @@ const TrailerVideo: FC<TrailerVideoProps> = ({
     return () => window.removeEventListener("keydown", closeModal);
   }, [setPlaying]);
 
+  const handleClose = () => setPlaying(false);
+
   return (
     <div
       className={twMerge(
+        "w-full flex items-center justify-center",
         banner
-          ? "w-screen h-screen bg-black z-10"
+          ? "w-screen h-screen flex bg-black z-10"
           : "fixed inset-0 flex items-center justify-center backdrop-brightness-[10%] z-50"
       )}
     >
-      <CenteredElement>
-        <div className="absolute w-[80vw] h-[80vh] -mt-20 tablet:-mt-6">
-          <iframe
-            src={`https://www.youtube.com/embed/${videoKey}?autoplay=1`}
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-            title="YouTube Video"
-            className="absolute top-0 left-0 w-full h-full"
-          ></iframe>
-        </div>
-      </CenteredElement>
+      <div className="absolute w-[90%] sm:w-[95%] lg:w-[80%] xl:w-[75%] h-[75vh] -mt-20 tablet:-mt-6">
+        <Image
+          className="hover:cursor-pointer hover:scale-110 transition-all duration-200 ease-in-out z-50 ml-auto"
+          src={close}
+          alt="close icon"
+          onClick={handleClose}
+        />
+        <iframe
+          src={`https://www.youtube.com/embed/${videoKey}?autoplay=1`}
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+          title="YouTube Video"
+          className="absolute top-10 left-0 w-full h-full"
+        ></iframe>
+      </div>
     </div>
   );
 };
