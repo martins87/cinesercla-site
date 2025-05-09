1b6795c1f8f3bc038a3ba17185e26e426de895f9
Daniel Martins <martinsda@gmail.com>
2025-05-09 10:52:28 -0300

    edit skeleton styling

--- app/components/Movies/MovieCardsSkeleton.tsx
+++ app/components/Movies/MovieCardsSkeleton.tsx
@@ -11,7 +11,7 @@ const MovieCardsSkeleton: FC<MovieCardsSkeletonProps> = ({ children }) => {
       {Array.from({ length: 4 }).map((_, index: number) => (
         <div
           key={index}
-          className="w-full flex flex-col gap-y-2 bg-[#3B3B3B] rounded-2xl p-4"
+          className="w-full flex flex-col gap-y-2 bg-[#D8D8D8] dark:bg-[#49575A] rounded-2xl p-4"
         >
           <div className="relative min-h-[344px] max-h-[344px] lg:min-h-[376px] lg:max-h-[376px] xl:min-h-[440px] xl:max-h-[440px] flex flex-1 object-center">
             {children}
