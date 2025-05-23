b5774ce49329f62bbc25dffa85ffca63cbe541d2
Daniel Martins <martinsda@gmail.com>
2025-05-23 16:49:52 -0300

    edit bomboniere card image quality

--- app/components/BomboniereCard.tsx
+++ app/components/BomboniereCard.tsx
@@ -12,13 +12,13 @@ type BomboniereCardProps = {
 const BomboniereCard: FC<BomboniereCardProps> = ({ product }) => {
   return (
     <CenteredEl className="relative h-80 p-4 rounded-[32px] transition-all ease-in-out duration-300 bg-[#3B3B3B] hover:bg-[#3B3B3B]/90">
-      <Image
-        className="h-56"
-        width={94}
-        height={140}
-        src={`/api/files/${product.imageFileId}`}
-        alt="bomboniere item"
-      />
+      <div className="relative w-64 aspect-square">
+        <Image
+          fill
+          src={`/api/files/${product.imageFileId}`}
+          alt="bomboniere item"
+        />
+      </div>
       <CenteredEl
         className="absolute w-4/5 h-[84px] bottom-6 px-4 py-2 rounded-2xl bg-[#D9D9D9]"
         direction="col"
