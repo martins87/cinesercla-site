396911b838c9804f86b208ad15662194a68e1ad8
Daniel Martins <martinsda@gmail.com>
2025-06-03 07:00:52 -0300

    edit programacao styling

--- app/components/Movie/Auditorium.tsx
+++ app/components/Movie/Auditorium.tsx
@@ -13,8 +13,10 @@ const Auditorium: FC<AuditoriumProps> = ({ number, time, small }) => {
   return (
     <CenteredElement
       className={twMerge(
-        "w-24 lg:w-32 p-3 lg:p-4 bg-white/45 rounded-xl transition-all duration-200 hover:scale-105 ease-in-out",
-        small ? "h-fit items-center" : "h-28 lg:h-36 items-start"
+        "bg-white/45 rounded-xl transition-all duration-200 hover:scale-105 ease-in-out",
+        small
+          ? "w-20 lg:w-28 p-2 lg:p-3 h-fit items-center"
+          : "w-24 lg:w-32 p-3 lg:p-4 h-28 lg:h-36 items-start"
       )}
       direction="col"
       justify="between"
@@ -24,7 +26,12 @@ const Auditorium: FC<AuditoriumProps> = ({ number, time, small }) => {
           Sala {number}
         </Typography>
       )}
-      <Typography className="text-2xl lg:text-4xl" weight="700">
+      <Typography
+        className={twMerge(
+          small ? "text-xl lg:text-2xl" : "text-2xl lg:text-4xl"
+        )}
+        weight="700"
+      >
         {time}
       </Typography>
     </CenteredElement>
